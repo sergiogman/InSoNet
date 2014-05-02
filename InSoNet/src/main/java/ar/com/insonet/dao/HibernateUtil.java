de0e90583601f24cc7ef41e7ca0cf31b9f295db1
rodolfotallerweb2 <inturias_r@yahoo.com.ar>
2014-05-02 17:25:59 -0300

    cambios en hibernate.cfg.xml

--- InSoNet/src/main/java/ar/com/insonet/dao/HibernateUtil.java
+++ InSoNet/src/main/java/ar/com/insonet/dao/HibernateUtil.java
@@ -2,6 +2,7 @@ package ar.com.insonet.dao;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
+//import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	
